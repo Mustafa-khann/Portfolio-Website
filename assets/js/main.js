0b61b26e929b93c6c7326224f3da52374aa9e5fd
Mustafa <mkafridi073@gmail.com>
2021-08-11 21:42:49 +0500

    added completed projects in portfolio

--- assets/js/main.js
+++ assets/js/main.js
@@ -91,3 +91,26 @@ $(document).ready(function ()
         }
     })
 })
+
+var buffer = 20; //scroll bar buffer
+var iframe = document.getElementById('iframe');
+
+function pageY(elem) {
+    return elem.offsetParent ? (elem.offsetTop + pageY(elem.offsetParent)) : elem.offsetTop;
+}
+
+function resizeIframe() {
+    var height = document.documentElement.clientHeight;
+    height -= pageY(document.getElementById('ifm'))+ buffer ;
+    height = (height < 0) ? 0 : height;
+    document.getElementById('ifm').style.height = height + 'px';
+}
+
+// .onload doesn't work with IE8 and older.
+if (iframe.attachEvent) {
+    iframe.attachEvent("onload", resizeIframe);
+} else {
+    iframe.onload=resizeIframe;
+}
+
+window.onresize = resizeIframe;
