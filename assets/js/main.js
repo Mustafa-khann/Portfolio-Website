8f52c7c4d8ab8cc1ee0da15b2a4e22a2a12b9287
Mustafa <mkafridi073@gmail.com>
2021-10-25 10:51:56 +0500

    update in js animations

--- assets/js/main.js
+++ assets/js/main.js
@@ -27,38 +27,38 @@ navLink.forEach(n => n.addEventListener('click', linkAction))
 
 /*===== Scroll reveal Animation =====*/
 const sr = ScrollReveal({
-        origin: 'right',
-        distance: '-80px',
+        origin: 'left',
+        distance: '50px',
         duration: 2500,
         reset: true
 
     })
 /*===== Scroll Home =====*/
 sr.reveal('.home__title',{})
-sr.reveal('.button',{delay:200})
-sr.reveal('.home__img',{delay:400, origin: 'left'})
+sr.reveal('.button',{delay:200, origin: 'right'})
+sr.reveal('.home__img',{delay:400})
 sr.reveal('.home__social-icon',{interval: 200, origin: 'left'})
 /*===== Scroll About =====*/
 sr.reveal('.about__img',{})
 sr.reveal('.about__subtitle',{delay:200})
-sr.reveal('.text',{delay:200, origin: 'left'})
+sr.reveal('.text',{delay:200, origin: 'right'})
 /*===== Scroll Interest =====*/
-sr.reveal('.row',{delay: 400, origin:'left'})
+sr.reveal('.row',{delay: 400, origin:'right'})
 /*===== Scroll Skills =====*/
 sr.reveal('.skills__title',{})
 sr.reveal('.skills__subtitle',{delay:200})
 sr.reveal('.professional__skills__title',{delay:100})
 sr.reveal('.professional__skills__subtitle',{delay:200})
 sr.reveal('.professional__skills',{delay:300})
-sr.reveal('.skills__data', {delay:400, origin: 'left'});
+sr.reveal('.skills__data', {delay:400, origin: 'right'});
 sr.reveal('.skills__img',{delay:450, origin: 'top'})
 /*===== Scroll Portfolio =====*/
 sr.reveal('.work__img',{interval:200, origin: 'top'})
 /*===== Scroll Contact =====*/
-sr.reveal('.contact__name',{delay:100, origin: 'left'})
-sr.reveal('.contact__email',{delay:200, origin: 'left'})
-sr.reveal('.contact__message',{delay:400, origin: 'left'})
-sr.reveal('.submit', {delay:500, origin: 'left'})
+sr.reveal('.contact__name',{delay:100, origin: 'right'})
+sr.reveal('.contact__email',{delay:200, origin: 'right'})
+sr.reveal('.contact__message',{delay:400, origin: 'right'})
+sr.reveal('.submit', {delay:500, origin: 'right'})
 
 /*===== Form resubmission appearance disable after every refresh of the page =====*/
 if ( window.history.replaceState ) {
